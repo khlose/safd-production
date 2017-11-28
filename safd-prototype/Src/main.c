a836b5cbd20143fdd4df58d08f9fafb15ca096eb
khlose <schwin60@hotmail.com>
2017-11-28 16:59:26 -0500

    debugging still show no result

--- safd-prototype/Src/main.c
+++ safd-prototype/Src/main.c
@@ -127,19 +127,21 @@ int main(void)
   HAL_GPIO_WritePin(LED_D4_PORT,LED_D4_PIN,GPIO_PIN_RESET);
   uint8_t val = 0;
 
-  DS2782Status return_status;
+  DS2782Status return_status = ds2782_init(&hi2c2);
+
   //return_status = readStatusReg(&hi2c2,&val);
   //return_status = ds2782_init(&hi2c2);
 
   HAL_StatusTypeDef status;
-
   uint8_t slave_Addr = 0;
+
+/*
 
   for(uint8_t addr=0;addr<=0xFF;addr++)
   {
 	  if(addr != 0xd4 && addr != 0xd5)
 	  {
-		  status = HAL_I2C_Mem_Read(&hi2c2,addr,0x7E,I2C_MEMADD_SIZE_8BIT,&slave_Addr,1,100);
+		  status = HAL_I2C_Mem_Read(&hi2c2,addr,0x7E,I2C_MEMADD_SIZE_8BIT,&slave_Addr,1,1000);
 		  if(status == HAL_OK)
 		  {
 			  HAL_GPIO_WritePin(LED_D5_PORT,LED_D5_PIN,GPIO_PIN_SET);
@@ -147,7 +149,7 @@ int main(void)
 	  }
   }
 
-
+*/
 
   /* USER CODE END 2 */
 
